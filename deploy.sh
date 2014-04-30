61484229ac560cf631bbccc97273042457acc52b
Moshe Bergman <bergmanm@gmail.com>
2014-04-29 18:18:02 -0700

    once more

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 # global install: nodejs, gulp, forever, coffee-script, bower
 # copy config
-sudo service stop site-journal
+sudo stop site-journal
 npm install --production
 bower install --config.interactive=false
 gulp build
 NODE_ENV=production forever start -c coffee journal-server.coffee
-sudo service start site-journal
+sudo start site-journal
